ca5854c5ad3625e061524c25d3694604a5ce4a1f
sididev96 <sejjid.gashi@gmail.com>
2022-10-26 18:48:55 +0200

    birthdaycake

---
+++ birthday.js
@@ -0,0 +1,14 @@
+let sumCakes = 0;
+const Birthday = (KayoSick) => {
+  return new Promise((resolve, reject) => {
+    if (!KayoSick) {
+      resolve(sumCakes++);
+    }
+  });
+};
+for (i = 0; i < 14; i++) {
+  let sickNotSick = Math.random() < 0.5;
+  Birthday(sickNotSick);
+}
+sick = 14 - sumCakes;
+console.log("Cakes made:" + sumCakes, "sick: " + sick);
