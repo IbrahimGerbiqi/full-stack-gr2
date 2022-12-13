8ae0582128bb2f452ba0c532c7c1e66e17696e84
ErgonSyla <ergonsyla8@gmail.comj>
2022-12-13 19:01:22 +0100

    sen

--- database/database.js
+++ database/database.js
@@ -1,31 +1,30 @@
 // const mysql = require('mysql')
 
-<<<<<<< HEAD
+ HEAD
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'school'
   })
-=======
+
 // const connection = mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
 //     password: 'pw123456',
 //     database: 'full-stack'
 //   })
->>>>>>> 8deeae402f61f0da54ec5caa0f7c34a96978baed
 
 //   connection.connect((err)=>{
 //     if(err) throw err
 //     console.log("MySQL database is connected successfully!");
 //   })
 
-// module.exports = connection
+ // module.exports = connection
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('full-stack', 'root', 'pw123456', {
+const sequelize = new Sequelize('school', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql'
 });
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const db = require('./models')
 const app = express()
 app.use(express.json())
 
+app.get('/students', (req,res) => {
+    db.query('SELECT * FROM students', (error, result) => {
+        if (error) throw error;
+ 
+        res.send(result);
+    });
+});
+
+
+app.delete('/students/:id',(req,res)=>{
+    db.query('DELETE * from students where id = 12')
+    .then( userResponse => {
+      res.json( userResponse )
+    })
+})
 
 app.listen(3000)
