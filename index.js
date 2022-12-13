f7093f1fb1479319b34fa5b7a6871754390d80e2
Ibrahim <ibrahimgerbiqi99@gmail.com>
2022-12-13 14:35:49 +0100

    exam

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const app = express()
-
-
-const models = require('./models/index');
 app.use(express.json())
-
-const userRouters = require('./routers/users')
-
-app.use('/api/users', userRouters)
-
 
 
 app.listen(3000)
