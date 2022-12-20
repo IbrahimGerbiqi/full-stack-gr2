97b80e1b47765e1975d1cbd1e2af4142dacaef0e
Amin Durguti <amindurguti123@gmail.com>
2022-12-20 17:56:12 +0100

    basic angular implementation

---
+++ client/full-stack-gr2/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login', component: LoginComponent},
+  {path:'notFound',component:NotFoundComponent},
+  {path:"**",redirectTo: "not-Found"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
