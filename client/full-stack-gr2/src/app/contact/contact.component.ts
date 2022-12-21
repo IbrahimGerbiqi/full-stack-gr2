9cd84ab71d2ec0272dfa35a4b30926e9189a41ae
Ibrahim <ibrahimgerbiqi99@gmail.com>
2022-12-21 19:27:15 +0100

    ngModel

--- client/full-stack-gr2/src/app/contact/contact.component.ts
+++ client/full-stack-gr2/src/app/contact/contact.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  firstName:string='';
+  lastName:string='';
+  age:number=0;
+  isUpdate = false
+  userId = 0
+
   artist = 'Nora Istrefi';
   studentet = [
     {
@@ -26,4 +32,44 @@ export class ContactComponent {
     }
   ];
 
+  remove(index:number){
+      this.studentet.splice(index,1)
+      this.isUpdate = false
+  }
+
+  update(index:number){
+    this.firstName = this.studentet[index].emri
+    this.lastName = this.studentet[index].mbiemri
+    this.age = this.studentet[index].mosha
+    this.isUpdate = true
+    this.userId = index
+  }
+
+  submit(){
+   if(!this.isUpdate){
+    this.studentet.push({
+      emri:this.firstName,
+      mbiemri:this.lastName,
+      mosha:this.age,
+    })
+   }else{
+    this.studentet[this.userId].emri = this.firstName
+    this.studentet[this.userId].mbiemri = this.lastName 
+    this.studentet[this.userId].mosha = this.age
+   }
+    this.resetFiels()
+  }
+
+  resetFiels(){
+    this.firstName = '';
+    this.lastName = ''
+    this.age = 0
+    this.isUpdate = false;
+    this.userId = -1
+  }
+
+
+
+
 }
+
